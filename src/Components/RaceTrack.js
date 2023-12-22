058ac07a90bd0771b69986ff9246b68021711d9e
Sreekanth Tirunagaram <101452992+sreekanth053@users.noreply.github.com>
2023-12-22 17:19:28 +0530

    Update RaceTrack.js

--- src/Components/RaceTrack.js
+++ src/Components/RaceTrack.js
@@ -25,15 +25,13 @@ const RaceTrack = ({ participants, onFinishRace }) => {
           <div className='race-container-3'>
           <div className='race-container-4'>
           <h2 >Race Track</h2>
-          <div type="time" style={{ margin:'10px' }}>Elapsed Time: {elapsedTime} seconds</div>
-        <p className="track-length">Track length 400m</p>
-        <button className="button" onClick={handleFinishRace}>Finish Race</button>
+          <div type="time" style={{ fontSize:"20px",fontWeight:"500",margin:'10px' }}>Elapsed Time: {elapsedTime} seconds</div>
+          <div className="track-length">Track length 400m</div>
+          <button className="button" onClick={handleFinishRace}>Finish Race</button>
           </div>
-          
           </div>
         </div>
       </div>
-      
     </div>
   );
 };
